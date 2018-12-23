8f603e29d913f0edfa0e099ca41357bd3970e51c
jouven <jouven@gmail.com>
2018-12-23 22:35:57 +0100

    Fix not capturing the process stdout/stderr

--- actionExecution/runProcessExecution.cpp
+++ actionExecution/runProcessExecution.cpp
@@ -111,7 +111,9 @@ void runProcessActionExecution_c::derivedExecute_f()
         actionProcess_pri.setCreateProcessArgumentsModifier([] (QProcess::CreateProcessArguments *args)
         {
             args->flags |= CREATE_NEW_CONSOLE;
-            args->startupInfo->dwFlags &= ~STARTF_USESTDHANDLES;
+            //args->startupInfo->hStdError = GetStdHandle(STD_ERROR_HANDLE);
+            //args->startupInfo->hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+            //args->startupInfo->dwFlags &= ~STARTF_USESTDHANDLES;
         });
 #endif
         //TODO issue an error on the "qt bug site" that even with a clear environment, no PATH set, it manages to run stuff in /usr/bin
